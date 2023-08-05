0aec67fdabad128d23a0ae5b4f78bec70e9a2b77
harshakhiltaylor <121634311+harshakhiltaylor@users.noreply.github.com>
2023-08-05 18:33:34 +0000

    Operations in js

---
+++ Basics/Operations.js
@@ -0,0 +1,17 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Harsh"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2"); // => 32
+console.log("1" + 2 + 2); // => 122
+
+console.log(+true); // 1
+console.log(+""); // 0 
