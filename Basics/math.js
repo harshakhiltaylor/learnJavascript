50c34c51399415683001074954f4fbbb32081cbe
harshakhiltaylor <121634311+harshakhiltaylor@users.noreply.github.com>
2023-08-06 17:51:43 +0000

    Maths in javascript

---
+++ Basics/math.js
@@ -0,0 +1,22 @@
+
+// Math in javascrpit is an object containing many functions
+
+console.log(Math.abs(-4)); // absolute or modulus function
+
+console.log(Math.round(6.3)); // roundOf Function 
+
+// Math.floor choose - lower value
+// Math.ceil choose - higher value
+
+console.log(Math.random());
+// will give the value between 0 and 1
+
+console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor((Math.random() * (max - min + 1))) + min); // will generate random values btw 
+// 10 and 20 
+
+
