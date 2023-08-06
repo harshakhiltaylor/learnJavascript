a6f79b31f59df4f0cc4cd03a9c642d4d5c75f2fa
harshakhiltaylor <121634311+harshakhiltaylor@users.noreply.github.com>
2023-08-06 06:17:43 +0000

    datatype summary

---
+++ Basics/summaryDataTypes.js
@@ -0,0 +1,21 @@
+// Prmitive
+
+//  7 Categories : string number boolean null undefined symbol BigInt  
+
+// Reference :(Non Primitive) 
+
+// Array Objects Functions
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+// Interview perspective
+
+
+// null typeof will be "Object"
+
+// object native does not implement => "Object"
+// object native or host doest implement  => "function"
