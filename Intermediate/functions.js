0e84f1527c74aa3f71b672326a41cc1c578fe07a
harshakhiltaylor <121634311+harshakhiltaylor@users.noreply.github.com>
2023-08-10 10:17:10 +0000

    Intro of functions in JS

---
+++ Intermediate/functions.js
@@ -0,0 +1,42 @@
+
+function  myName() {
+    console.log("Hello Harsh");
+}
+
+// myName - Reference  myName() -> Execute
+
+myName();
+
+function addNumbers(number1 , number2) {    // No need to give type of variable
+    console.log(number1 + number2);
+    return number1 + number2;   // return value if want to pass in a diiferent variable 
+}
+
+addNumbers(3,4); // 7
+addNumbers(3,"4");  // 34
+addNumbers("3","4");  // 34
+
+
+const result  = addNumbers(3,5);
+
+console.log("Result is :", result);
+
+function loggedInMessage(username)
+{    
+    // if(!username)
+
+    if(username === undefined)
+    {
+        console.log("Please enter the name of user!!");
+        return ;
+    }
+    else
+    {
+    return ` Person ${username} just logged in`;   // either print it or pass it in a variable 
+    }
+}
+
+
+console.log(loggedInMessage("harsh"));
+console.log(loggedInMessage());
+
