5fca7500ddbce3ac906e5f1ce660607ee081ca8c
harshakhiltaylor <121634311+harshakhiltaylor@users.noreply.github.com>
2023-08-11 05:12:49 +0000

    Scope JS

---
+++ Intermediate/scope.js
@@ -0,0 +1,13 @@
+var c = 300 
+// we do not use var because it does not follow scope property
+
+if(true)
+{
+    let a = 10
+    const b = 52
+    var c = 30     // var c is not following scope rules
+}
+
+//console.log(a);
+//console.log(b);
+console.log(c);
